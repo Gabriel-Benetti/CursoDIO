756201706c98dace1c97cae29c40a001dc74a152
Gabriel <gabrielf.ni10@gmail.com>
2024-04-14 12:36:12 -0300

    feito o desafio de POO

---
+++ CursoDio/Serializacao/Models/Vendas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializacao.Models
+{
+    internal class Vendas
+    {
+        public Vendas()
+        {
+            
+        }
+
+        public Vendas(int id, string produto, decimal valor)
+        {
+            Id = id;
+            Produto = produto;
+            Valor = valor;
+
+            
+        }
+        public int Id { get; set; }
+        public string Produto { get; set; }
+        public Decimal Valor { get; set; }
+    }
+}
---
+++ CursoDio/Serializacao/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Serializacao.Models;
+
+namespace Serializacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vendas v1 = new Vendas(1,"Carteira", 150.00M);
+            Vendas v2 = new Vendas(2,"Monitor", 1759.10M);
+            Vendas v3 = new Vendas(3,"Farofa", 6.98M);
+
+            List<Vendas> lista = new List<Vendas>();
+
+            lista.Add(v1);
+            lista.Add(v2);
+            lista.Add(v3);
+
+
+
+            string transformarJson = JsonConvert.SerializeObject(lista, Formatting.Indented);
+
+            Console.WriteLine(transformarJson);
+
+            File.WriteAllText("Teste.json", transformarJson);
+        }
+    }
+}
